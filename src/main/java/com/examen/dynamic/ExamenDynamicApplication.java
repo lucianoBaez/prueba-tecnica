df2a2d13654da1001847b22a1077ba30ac60d10e
Luciano  Baez <lutiano2@gmail.conm>
2021-03-23 23:01:35 -0300

    Inicio desarrollo prueba tecnica

---
+++ src/main/java/com/examen/dynamic/ExamenDynamicApplication.java
@@ -0,0 +1,11 @@
+package com.examen.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenDynamicApplication {
+    public static void main(String[] args) {
+	SpringApplication.run(ExamenDynamicApplication.class, args);
+    }
+}
